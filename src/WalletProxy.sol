39ef4bd19e509b5c6156c3653c72cc4b36ad6a7a
cqlyj <luoyingjie0721@gmail.com>
2025-03-23 15:58:10 +0800

    feat: add WalletProxy contract for upgradeable proxy functionality

---
+++ src/WalletProxy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC1967Utils} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
+
+contract WalletProxy is ERC1967Proxy {
+    /**
+     * @dev Initializes the upgradeable proxy with an initial implementation specified by `implementation`.
+     *
+     * If `_data` is nonempty, it's used as data in a delegate call to `implementation`. This will typically be an
+     * encoded function call, and allows initializing the storage of the proxy like a Solidity constructor.
+     *
+     * Requirements:
+     *
+     * - If `data` is empty, `msg.value` must be zero.
+     */
+    constructor(
+        address implementation,
+        bytes memory _data
+    ) payable ERC1967Proxy(implementation, _data) {
+        ERC1967Utils.upgradeToAndCall(implementation, _data);
+    }
+}
